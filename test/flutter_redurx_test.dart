9939b178a755d89437ff3b679e3aa93f518c3ea2
leocavalcante <lc@leocavalcante.com>
2018-08-02 23:58:37 -0300

    Add more tests

--- test/flutter_redurx_test.dart
+++ test/flutter_redurx_test.dart
@@ -3,26 +3,47 @@ import 'package:flutter_redurx/flutter_redurx.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 class State {
-  State(this.count);
+  State([this.count = 0]);
   final int count;
 }
 
-class Fixture extends StatelessWidget {
-  Fixture({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-void main() {
-  final state = State(0);
-  final store = Store(state);
-
+void main() async {
   testWidgets('Provider should build its child.', (tester) async {
-    final fixture = Fixture();
+    final store = Store(State());
+    final fixture = Container();
+
     await tester.pumpWidget(Provider(store: store, child: fixture));
+
     expect(find.byWidget(fixture), findsOneWidget);
+  });
+
+  testWidgets('Connect\'s null value Widget is a Container.', (tester) async {
+    final store = Store(State(0));
+
+    final fixture = Connect<State, int>(
+      convert: (state) => state.count,
+      where: (prev, next) => next != prev,
+      builder: (count) => Text(count.toString()),
+    );
+
+    await tester.pumpWidget(Provider(store: store, child: fixture));
+
+    expect(find.byType(Container), findsOneWidget);
+  });
+
+  testWidgets('Connect should build with props.', (tester) async {
+    final store = Store(State(0));
+
+    final fixture = Connect<State, int>(
+      convert: (state) => state.count,
+      where: (prev, next) => next != prev,
+      builder: (count) =>
+          Text(count.toString(), textDirection: TextDirection.ltr),
+    );
+
+    await tester.pumpWidget(Provider(store: store, child: fixture));
+    await tester.pump(Duration.zero);
+
+    expect(find.text('0'), findsOneWidget);
   });
 }
