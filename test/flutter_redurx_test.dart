658a5f72a49d2970fb09d49fbfe417b0f9f7f469
leocavalcante <lc@leocavalcante.com>
2018-08-03 00:31:35 -0300

    Even more tests

--- test/flutter_redurx_test.dart
+++ test/flutter_redurx_test.dart
@@ -7,6 +7,31 @@ class State {
   final int count;
 }
 
+class Increment extends Action<State> {
+  @override
+  State reduce(State state) => State(state.count + 1);
+}
+
+class Counter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Row(
+        children: <Widget>[
+          Connect<State, int>(
+            convert: (state) => state.count,
+            where: (prev, next) => next != prev,
+            builder: (counter) => Text(counter.toString()),
+          ),
+          RaisedButton(
+            onPressed: () => Provider.dispatch<State>(context, Increment()),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 void main() async {
   testWidgets('Provider should build its child.', (tester) async {
     final store = Store(State());
@@ -45,5 +70,41 @@ void main() async {
     await tester.pump(Duration.zero);
 
     expect(find.text('0'), findsOneWidget);
+  });
+
+  testWidgets('Connect should rebuild if props has changed.', (tester) async {
+    final store = Store(State(0));
+
+    final fixture = Connect<State, int>(
+      convert: (state) => state.count,
+      where: (prev, next) => next != prev,
+      builder: (count) =>
+          Text(count.toString(), textDirection: TextDirection.ltr),
+    );
+
+    await tester.pumpWidget(Provider(store: store, child: fixture));
+    await tester.pump(Duration.zero);
+
+    expect(find.text('0'), findsOneWidget);
+
+    store.dispatch(Increment());
+    await tester.pump(Duration.zero);
+
+    expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('Provider should dispatch actions.', (tester) async {
+    final store = Store(State(0));
+    final fixture = Counter();
+
+    await tester.pumpWidget(Provider(store: store, child: fixture));
+    await tester.pump(Duration.zero);
+
+    expect(find.text('0'), findsOneWidget);
+
+    await tester.tap(find.byType(RaisedButton));
+    await tester.pump(Duration.zero);
+
+    expect(find.text('1'), findsOneWidget);
   });
 }
