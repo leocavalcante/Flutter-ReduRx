a267bbcdeb5908e84f9190ade24dd4185c07505d
leocavalcante <lc@leocavalcante.com>
2019-02-09 01:10:56 -0200

    Add DocBlocks

--- lib/src/connect_widget.dart
+++ lib/src/connect_widget.dart
@@ -1,11 +1,18 @@
+/// Behaves just like Connect but is extendable. Very useful when you see yourself in a Widget with a lot of Connects.
 import 'package:flutter/widgets.dart';
 import 'package:flutter_redurx/flutter_redurx.dart';
 
 abstract class ConnectWidget<S, P> extends StatefulWidget {
+  /// [convert] is how you map the State to [builder] props.
   P convert(S state);
+
+  /// With [where] you can filter when the Widget should re-render, this is very important!
   bool where(P oldProps, P newProps) => newProps != oldProps;
+
+  /// If you want to handle [null] values on the [builder] by yourself, set [nullable] to [true].
   Widget build(BuildContext context, P props);
 
+  /// Sugar to dispatch actions without calling the Provider.
   void dispatch(BuildContext context, ActionType action) => Provider.dispatch<S>(context, action);
 
   @override
